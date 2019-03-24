12c3d2a307adcee2b4553c90f4f628975777ba2b
Sade Sirén <fyrwill@gmail.com>
2019-03-24 22:32:24 +0200

    portfolio ver1 done

--- client.js
+++ client.js
@@ -1,4 +1,46 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    //do work
-    document.getElementById('test').innerHTML = 'This is inserted';
+document.addEventListener('DOMContentLoaded', () => {
+    // hooks are now available
+
+    // get modifiable elements
+    const smallmenu = document.getElementById('smallmenu');
+    const smallmenuclose = document.querySelectorAll('a.smallmenuclose');
+    const photogrid = document.querySelectorAll('div.w3-third > img');
+    const lightbox = document.getElementById('lightbox');
+
+    // opening the lightbox when a portfolio photo is clicked
+    photogrid.forEach(photo => {
+        photo.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.getElementById("img01").src = photo.src;
+            document.getElementById("lightbox").style.display = "block";
+            const headingText = document.getElementById("portfolio-heading");
+            headingText.innerHTML = photo.alt;
+            const captionText = document.getElementById("portfolio-caption");
+            captionText.innerHTML = photo.getAttribute('data-caption');
+        });
+    })
+
+    // closing the lightbox
+    lightbox.addEventListener('click', (e) => {
+        e.preventDefault();
+        lightbox.style.display = 'none';
+    });
+
+    // Scripts to open and close sidebar when screen is small
+    smallmenu.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("myOverlay").style.display = "block";
+    });
+
+    smallmenuclose.forEach(close => {
+        close.addEventListener('click', (e) => {
+            document.getElementById("mySidebar").style.display = "none";
+            document.getElementById("myOverlay").style.display = "none";
+        });
+    });
 });
+
+/**
+ * hjhguhihjuyjuy
+ */
